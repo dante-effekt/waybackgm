d0c49915d15fdce8e0ceec468d6fe37c8b57272c
DanielHexe <CABALLERO.JUAN.3B@gmail.com>
2023-12-03 16:16:05 -0600

    Menu con los botones off/reset update

--- apagado.py
+++ apagado.py
@@ -8,24 +8,24 @@ def apagar():
 def reiniciar():
     os.system("reboot")
 
-# Crear la ventana principal
-ventana = tk.Tk()
-ventana.title("Menú de Apagado")
-ventana.attributes("-fullscreen", True)
+# Crear la app principal
+app = tk.Tk()
+app.title("Menú de Apagado")
+app.attributes("-fullscreen", True)
 
 # Íconos
 icono_apagar = PhotoImage(file="./icons/apagadoN.png")  # Reemplaza "apagar_icono.png" con la ruta de tu propio ícono
 icono_reiniciar = PhotoImage(file="./icons/restartN.png")  # Reemplaza "reiniciar_icono.png" con la ruta de tu propio ícono
 
 # Botón de apagar
-btn_apagar = tk.Button(ventana, text="Apagar", command=apagar, bg="red", fg="white", image=icono_apagar, compound="left")
+btn_apagar = tk.Button(app, text="Apagar", command=apagar, bg="red", fg="white", image=icono_apagar, compound="left")
 btn_apagar.pack(pady=10)
 
 # Botón de reiniciar
-btn_reiniciar = tk.Button(ventana, text="Reiniciar", command=reiniciar, bg="yellow", fg="black", image=icono_reiniciar, compound="left")
+btn_reiniciar = tk.Button(app, text="Reiniciar", command=reiniciar, bg="yellow", fg="black", image=icono_reiniciar, compound="left")
 btn_reiniciar.pack(pady=10)
 
 # Ejecutar el bucle de la interfaz gráfica
-ventana.mainloop()
+app.mainloop()
 
 
--- menu4.py
+++ menu4.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from tkinter import PhotoImage
 import subprocess
 import time
 import threading
@@ -66,6 +67,10 @@ def on_clic_izquierdo():
         seleccionado = menu.get(selected_index[0])
         ejecutar_snes9x(seleccionado)
 
+def cerrar_ventana():
+    app.destroy()
+    os.system("exit")
+    sys.exit(1)
 
 def salir(event):
     app.destroy()
@@ -192,12 +197,30 @@ def actualizar_lista_archivos():
     for archivo in archivos:
         menu.insert(tk.END, archivo)
 
+def apagar():
+    os.system("shutdown now")
+
+def reiniciar():
+    os.system("reboot")
+
+
 ventanas = {}
 
 archivos = cargar_archivos()
 
 app = tk.Tk()
 app.attributes("-fullscreen", True)
+icono_apagar = PhotoImage(file="./icons/apagadoScale.png")  # Reemplaza "apagar_icono.png" con la ruta de tu propio ícono
+icono_reiniciar = PhotoImage(file="./icons/restartScale.png")  # Reemplaza "reiniciar_icono.png" con la ruta de tu propio ícono
+
+
+# Botón de apagar
+btn_apagar = tk.Button(app, text="Apagar", command=apagar, bg="red", fg="white", image=icono_apagar, compound="left")
+btn_apagar.pack(side=tk.LEFT, padx=10, pady=10)
+
+# Botón de reiniciar
+btn_reiniciar = tk.Button(app, text="Reiniciar", command=reiniciar, bg="yellow", fg="black", image=icono_reiniciar, compound="left")
+btn_reiniciar.pack(side=tk.LEFT, padx=10, pady=10)
 
 menu = tk.Listbox(app, selectbackground="lightblue", bg="white", fg="black", font=("Courier", 14))
 menu.pack(side="left", fill="both", expand=True)
