2c82f178e9d7d23bf6223d57434e00e34192ed9b
Dante <dante_leoarg@hotmail.com>
2023-12-02 19:52:59 -0600

    Detección Automática de USB

--- prueba_menu_c2.py
+++ prueba_menu_c2.py
@@ -12,13 +12,17 @@ import time
 import threading
 import psutil
 import shutil
+import pyudev
+import subprocess as sp
 
 directorio_actual = os.getcwd()
 directorio = directorio_actual
+usb = 0  # Flag para saber si hay una USB conectada
+dispositivo = 0  # Mutará de int a objeto device cuando se inserte una USB
 
 def ejecutar_snes9x(nombre_archivo):
     ruta_archivo = os.path.join(directorio, nombre_archivo)
-    comando = f"-fullscreen snes9x {ruta_archivo}"
+    comando = f"/home/fipy/Documents/snes9x-1.60/gtk/build/snes9x-gtk {ruta_archivo}"
     subprocess.run(comando, shell=True)
 
 def cargar_archivos():
@@ -32,45 +36,93 @@ def on_seleccion(event):
 def salir(event):
     app.destroy()
 
+
+#Definimos las funciones necesarias para detectar la USB
+def dev_stats(path):
+    photos = []
+    for file in os.listdir(path):
+        if file.endswith(".jpg") \
+        or file.endswith(".png"):
+            photos.append(path+"/"+file)
+    print("{} has {} photos.".format(path, len(photos)))
+    return photos
+
+
+def print_dev_info(device):
+    print("Device sys_path: {}".format(device.sys_path))
+    print("Device sys_name: {}".format(device.sys_name))
+    print("Device sys_number: {}".format(device.sys_number))
+    print("Device subsystem: {}".format(device.subsystem))
+    print("Device device_type: {}".format(device.device_type))
+    print("Device is_initialized: {}".format(device.is_initialized))
+
+
+def auto_mount(path):
+    args = ["udisksctl", "mount", "-b", path]
+    sp.run(args)
+
+
+def get_mount_point(path):
+    args = ["findmnt", "-unl", "-S", path]
+    cp = sp.run(args, capture_output=True, text=True)
+    out = cp.stdout.split(" ")[0]
+    return out
+
+
+def check_dev_events(action, device): #Funcion manejadora de eventos
+    global usb
+    global dispositivo
+    if (action == "add"): #Si se añade una usb
+        usb = 1
+        print_dev_info(device)
+        dispositivo = device #Pasar el objeto dispositivo a la variable global
+    if (action == "remove"): #Si se retira una usb
+        usb = 0
+
+context = pyudev.Context()
+monitor = pyudev.Monitor.from_netlink(context)
+monitor.filter_by(subsystem="block", device_type="partition")        
+observer = pyudev.MonitorObserver(monitor, check_dev_events, name='monitor-observer')
+observer.daemon #MonitorObserver sera un hilo demonio
+observer.start()
+
+
 def detectar_usb():
-    dispositivos_previos = set(psutil.disk_partitions())
-
+    global dispositivo
     while True:
-        time.sleep(1)
-        dispositivos_actuales = set(psutil.disk_partitions())
+        if (usb==0):
+            print("NO se ha ingresado un usb")
+            time.sleep(1)
+        if (usb==1):
+            usb_nuevo = get_mount_point("/dev/" + dispositivo.sys_name)
+            print("La ruta del dispositivo es: " + usb_nuevo)
+            mostrar_contenido_usb(usb_nuevo)
 
-        dispositivo_nuevo = dispositivos_actuales - dispositivos_previos
-        dispositivo_nuevo_condicion = len(dispositivos_actuales) - len(dispositivos_previos)
-        if dispositivo_nuevo_condicion == 1:
-            dispositivo_nuevo = dispositivo_nuevo.pop()
-            # Mostrar la ventana desde el hilo principal
-            app.after(0, lambda: mostrar_contenido_usb(dispositivo_nuevo))
-            dispositivo_nuevo_condicion = 0
 
-        dispositivos_previos = dispositivos_actuales
-
-def mostrar_contenido_usb(dispositivo):
-    ruta_dispositivo = os.path.join(dispositivo.device, '')
+def mostrar_contenido_usb(ruta_dispositivo):
+    global dispositivo
+    #ruta_dispositivo = os.path.join(dispositivo, ruta_dispositivo)
     # Copiar archivos desde la USB al directorio
     app.after(0, lambda: copiar_archivos_smc(ruta_dispositivo))
     
-    if dispositivo.device in ventanas:
+    if dispositivo in ventanas:
         return
     
+    print("La ruta del dispositivo es: " + ruta_dispositivo)
     archivos = [archivo for archivo in os.listdir(ruta_dispositivo) if archivo.endswith('.smc')]
     ventana_usb = tk.Toplevel()
     ventana_usb.overrideredirect(True)
 
-    mensaje = f'Archivos en {dispositivo.device}:\n\n'
+    mensaje = f'Archivos en {dispositivo}:\n\n'
     mensaje += '\n'.join(archivos)
 
     etiqueta = tk.Label(ventana_usb, text=mensaje, justify='left', anchor='w')
     etiqueta.pack()
 
-    ventanas[dispositivo.device] = ventana_usb
+    ventanas[dispositivo] = ventana_usb
 
     def cerrar_ventana(event):
-        ventanas.pop(dispositivo.device, None)
+        ventanas.pop(dispositivo, None)
         ventana_usb.destroy()
         actualizar_lista_archivos()
 
@@ -127,6 +179,7 @@ menu.bind("<ButtonRelease-1>", on_seleccion)
 app.bind("<Escape>", salir)
 
 hilo_usb = threading.Thread(target=detectar_usb)
+hilo_usb.daemon
 hilo_usb.start()
 
 app.mainloop()
