fc9741ade4fdab0c59bd9d34d56a361e1186422e
DanielHexe <CABALLERO.JUAN.3B@gmail.com>
2023-12-03 14:43:36 -0600

    Apagado

---
+++ apagado.py
@@ -0,0 +1,31 @@
+import tkinter as tk
+from tkinter import PhotoImage
+import os
+
+def apagar():
+    os.system("shutdown now")
+
+def reiniciar():
+    os.system("reboot")
+
+# Crear la ventana principal
+ventana = tk.Tk()
+ventana.title("Menú de Apagado")
+ventana.attributes("-fullscreen", True)
+
+# Íconos
+icono_apagar = PhotoImage(file="./icons/apagadoN.png")  # Reemplaza "apagar_icono.png" con la ruta de tu propio ícono
+icono_reiniciar = PhotoImage(file="./icons/restartN.png")  # Reemplaza "reiniciar_icono.png" con la ruta de tu propio ícono
+
+# Botón de apagar
+btn_apagar = tk.Button(ventana, text="Apagar", command=apagar, bg="red", fg="white", image=icono_apagar, compound="left")
+btn_apagar.pack(pady=10)
+
+# Botón de reiniciar
+btn_reiniciar = tk.Button(ventana, text="Reiniciar", command=reiniciar, bg="yellow", fg="black", image=icono_reiniciar, compound="left")
+btn_reiniciar.pack(pady=10)
+
+# Ejecutar el bucle de la interfaz gráfica
+ventana.mainloop()
+
+
